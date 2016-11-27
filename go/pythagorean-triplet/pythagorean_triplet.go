7aebf89718798a09cbbb42ffedb9dfafacc970f3
Michael Orr <michael@orr.co>
2016-11-26 19:22:31 -0500

    pythagorean solution

--- go/pythagorean-triplet/pythagorean_triplet.go
+++ go/pythagorean-triplet/pythagorean_triplet.go
@@ -1,11 +1,41 @@
 package pythagorean
 
+import (
+	"math"
+)
+
 type Triplet [3]int
 
 func Range(min, max int) []Triplet {
-	return make([]Triplet, 1)
+	var result []Triplet
+
+	for a := min; a <= max; a++ {
+		a_sq := math.Pow(float64(a), 2)
+		for b := a + 1; b <= max; b++ {
+			b_sq := math.Pow(float64(b), 2)
+			for c := b + 1; c <= max; c++ {
+				c_sq := math.Pow(float64(c), 2)
+				if a_sq+b_sq == c_sq {
+					result = append(result, Triplet{a, b, c})
+				}
+			}
+		}
+	}
+	return result
 }
 
 func Sum(p int) []Triplet {
-	return make([]Triplet, 1)
+	var result []Triplet
+	for a := 1; a < p/3; a++ {
+		a_sq := math.Pow(float64(a), 2)
+		for b := a + 1; b < p; b++ {
+			b_sq := math.Pow(float64(b), 2)
+			c := p - (a + b)
+			c_sq := math.Pow(float64(c), 2)
+			if a_sq+b_sq == c_sq {
+				result = append(result, Triplet{a, b, c})
+			}
+		}
+	}
+	return result
 }
